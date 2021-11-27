039c67ed077f38b425e26eb63e6da69c40ff2e59
jmend <jmend736@gmail.com>
2021-11-26 19:35:44 -0500

    Update mkrt

--- fish/functions/mkrt.fish
+++ fish/functions/mkrt.fish
@@ -1,3 +1,28 @@
-function mkrt
-  pushd (mktemp -d /tmp/pg-XXXX)
+function mkrt --description 'Make a random temporary directory'
+  set -l args \
+    'j/java Make java-specific random temp' \
+    'c/complete Setup completions for mkrt' \
+    'h/help Show this help'
+
+  argparse (string split -f1 ' ' $args) -- $argv
+
+  if set -q _flag_help
+    string collect -- $args
+  else if set -q _flag_complete
+    for arg in $args
+      set -l arg_help (string split -m1 ' ' $arg)
+      set -l parts (string split '/' $arg_help[1])
+      complete -c mkrt -s$parts[1] -l$parts[2] -d"$arg_help[2]"
+    end
+  else if set -q _flag_java
+    mkrt
+    touch Main.java
+    echo '.PHONY: all
+      all:
+      	javac Main.java
+      	java Main' | string trim -c' ' >> Makefile
+  else
+    pushd (mktemp -d /tmp/pg-XXXX)
+  end
+
 end
