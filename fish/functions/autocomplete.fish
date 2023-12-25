9e87208599f235fd2fba13b8b0c7ba2166de54ab
jmend <jmend736@gmail.com>
2023-12-25 14:44:39 -0500

    add subcommand autocomplete example

---
+++ fish/functions/autocomplete.fish
@@ -0,0 +1,13 @@
+    switch $argv[1]
+        case subcommand1
+            echo executed subcommand1
+        case subcommand2
+            echo executed subcommand2
+        case _complete
+            set -l code (functions (status current-function) | string collect)
+            string match -r -a -q '\s*case (?<subcommands>[^_]\w+)' -- $code
+            complete -c (status current-function) \
+                -n "not __fish_seen_subcommand_from $subcommands" \
+                -xa "$subcommands"
+    end
+end
