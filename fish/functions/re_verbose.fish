0c79e736201878ef8d81f22cac8877e844c8c2bd
jmend <jmend736@gmail.com>
2024-03-03 19:10:24 -0500

    Add fish/re_verbose

---
+++ fish/functions/re_verbose.fish
@@ -0,0 +1,8 @@
+function re_verbose --description Strip\ regex\ of\ \#-comments\ and\ indentation.\ Like\ Python\'s\ re.VERBOSE.
+    set -l re__      '(?<re>.*?)'
+    set -l comment__ '(?<comment>(?:\s*#.*$))'
+    set -l indent__  '(?<indent>^\s*)'
+    string collect -- $argv |
+        string replace --regex "$indent__($re__)$comment__" '$re' $line |
+        string join --no-empty ''
+end
