7c4f11de4b7496032111aa4701e79bf73e147325
jmend <jmend736@gmail.com>
2024-04-23 09:22:40 -0400

    sync

--- fish/functions/comped.fish
+++ fish/functions/comped.fish
@@ -1,6 +1,4 @@
 function comped --wraps=functions --argument fname
-    if test -z "$fname"
-        return 1
-    end
-    vim ~/.config/fish/completions/$fname.fish
+    vim "~/.config/fish/completions/$fname.fish"
+    complete -c $fname -e
 end
--- fish/functions/mkrt.fish
+++ fish/functions/mkrt.fish
@@ -1,16 +1,3 @@
 function mkrt --description 'Make a random temporary directory'
-    switch $argv[1]
-        case make
-            set -l dir (mktemp -d /tmp/pg-XXXX)
-            ln -s -f $dir /tmp/pg-latest
-            pushd $dir
-        case _complete
-            set -l code (functions (status current-function) | string collect)
-            string match -r -a -q '\s*case (?<subcommands>[^_\'](\w|\.)+)' -- $code
-            complete -c (status current-function) \
-                -n "not __fish_seen_subcommand_from $subcommands" \
-                -xa "$subcommands"
-        case '*'
-            mkrt make
-    end
+    pushd (mktemp -d /tmp/pg-XXXX)
 end
