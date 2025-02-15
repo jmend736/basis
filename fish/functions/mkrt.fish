244cb2b2c57d8681d97f9186fdf193b37023ad6b
jmend <jmend736@gmail.com>
2025-02-15 12:09:52 -0500

    sync

--- fish/functions/mkrt.fish
+++ fish/functions/mkrt.fish
@@ -4,6 +4,15 @@ function mkrt --description 'Make a random temporary directory'
             set -l dir (mktemp -d /tmp/pg-XXXX)
             ln -s -f $dir /tmp/pg-latest
             pushd $dir
+        case gradle
+            mkrt make
+            gradle wrapper --gradle-version=8.8 $argv[2..]
+            ./gradlew init \
+                --type java-application \
+                --dsl groovy \
+                --package pg \
+                --project-name pg \
+                --test-framework junit-jupiter
         case _complete
             set -l code (functions (status current-function) | string collect)
             string match -r -a -q '\s*case (?<subcommands>[^_\'](\w|\.)+)' -- $code
