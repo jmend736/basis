8f674d0cee6c17e2e9c1e151eef69b08feb37c95
jmend <jmend736@gmail.com>
2025-03-14 12:24:58 -0400

    reap and sow

---
+++ fish/functions/sow.fish
@@ -0,0 +1,40 @@
+function sow
+    set -l helptext '
+    sow - Store a value, to be reaped later
+
+SYNOPSIS
+    COMMAND | sow [OPTIONS]
+    sow [OPTIONS] VALUES...
+
+EXAMPLES
+    $ sow value
+    $ reap
+    value
+
+    $ echo value | sow
+    $ reap
+    value
+
+DESCRIPTION
+    Store the passed in VALUE or read from stdin if no value arguments are
+    given. Can be accessed with reap from any fish instance.
+
+OPTIONS
+
+    *   ( -a | --append ) do not overwrite current value, append instead
+'
+    argparse -s a/append h/help -- $argv
+
+    if set -q _flag_help
+        echo $helptext
+        return
+    end
+
+    set -l args $argv
+    if test -z "$args"
+        while read line
+            set -a args $line
+        end
+    end
+    set $_flag_append -U _sowed $args
+end
