7550510c0c374a2cec3b79233b097e47daa3e1b3
jmend <jmend736@gmail.com>
2026-07-04 12:32:06 -0400

    sync

--- fish/functions/gradle-init.fish
+++ fish/functions/gradle-init.fish
@@ -1,43 +1,58 @@
 function gradle-init
-    argparse 'g/gradle=' 'j/java=' 'p/package=' -- $argv
-    set -l GRADLE_VERSION 9.1.0
+    argparse 'g/gradle=' 'j/java=' 'p/package=' 'd/dsl=' -- $argv
+
+    set -l GRADLE_VERSIONS 9.1.0
+    set -l GRADLE_VERSION $GRADLE_VERSIONS[1]
     if set -q _flag_gradle
         set GRADLE_VERSION $_flag_gradle
     end
-    set -l JAVA_VERSION 21
+
+    set -l JAVA_VERSIONS 21 25 17
+    set -l JAVA_VERSION $JAVA_VERSIONS[1]
     if set -q _flag_java
         set JAVA_VERSION $_flag_java
     end
-    set -l PACKAGE pg
+
+    set -l PACKAGES pg io.jmend
+    set -l PACKAGE $PACKAGES[1]
     if set -q _flag_package
         set PACKAGE $_flag_package
+    end
+
+    set -l DSLS kotlin groovy
+    set -l DSL $DSLS[1]
+    if set -q _flag_dsl
+        set DSL $_flag_dsl
     end
 
     set -l base_args \
         --overwrite \
         --no-incubating \
         --no-comments \
-        --dsl groovy \
+        --dsl $DSL \
         --java-version $JAVA_VERSION \
         --package $PACKAGE \
         --project-name $PACKAGE \
         --no-split-project \
 
     switch $argv[1]
-        case kotlin
+        case kotlin java
             gradle-init _wrapper
-            ./gradlew init \
-                $base_args \
-                --test-framework kotlintest \
-                --type kotlin-application
+            ./gradlew init (gradle-init args $argv[1])
             gradle-init _make
-        case java
-            gradle-init _wrapper
-            ./gradlew init \
-                $base_args \
-                --test-framework junit \
-                --type java-application
-            gradle-init _make
+        case args
+            switch $argv[2]
+                case java
+                    string collect -- $base_args \
+                        --test-framework junit \
+                        --type java-application
+                case kotlin ''
+                    string collect -- $base_args \
+                        --test-framework kotlintest \
+                        --type kotlin-application
+                case '*'
+                    echo "Invalid type"
+            end
         case _wrapper
             gradle wrapper --gradle-version=$GRADLE_VERSION
         case _make
@@ -55,7 +70,7 @@ function gradle-init
         case _complete
             set -l code (functions (status current-function) | string collect)
             string match --regex --all --quiet \
-                '\s*case (?<all_subcommands>( *[^_](\w|\.)+)+)' \
+                '\s{8}case (?<all_subcommands>( *[^_](\w|\.)+)+)' \
                 -- $code
             set -l subcommands (string split --no-empty ' ' $all_subcommands)
             complete -c (status current-function) \
@@ -64,15 +79,19 @@ function gradle-init
 
             complete -c (status current-function) \
                 -d "Gradle version" \
-                -s g -l gradle -xa "9.2.1"
+                -s g -l gradle -xa "$GRADLE_VERSIONS"
 
             complete -c (status current-function) \
                 -d "Java version" \
-                -s j -l java -xa "21 25"
+                -s j -l java -xa "$JAVA_VERSIONS"
+
+            complete -c (status current-function) \
+                -d "Gradle DSL" \
+                -s d -l dsl -xa "$DSLS"
 
             complete -c (status current-function) \
                 -d "Java pacakge" \
-                -s p -l package -xa "pg io.jmend"
+                -s p -l package -xa "$PACKAGES"
         case '*'
             echo "Invalid command: $argv[1]"
     end
